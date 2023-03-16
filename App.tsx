c4e9fa5e0791de22a589cdd09b5a3a54ac85fa1a
rapstar-surya <122983609+rapstar-surya@users.noreply.github.com>
2023-03-16 17:29:55 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h3>Soumya</h3>
+      <h3>7777777</h3>
+      <hr/>
+      <h3>sds@gmgm.com</h3>
+      <hr/>
+
+    </div>
+  );
+}
